eb2bd569d9ac7c1c5ee22929b3e46b46f19e166b
siam114 <smsiamislam598@gmail.com>
2025-09-22 00:35:05 +0600

    formating button compoennet

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -36,42 +36,40 @@ ButtonPrimary.propTypes = {
   classes: PropTypes.string,
 };
 
-
 // Button Outline
 const ButtonOutline = ({ href, target = "_self", label, icon, classes }) => {
-    if (href) {
-      return (
-        <a href={href} target={target} className={"btn btn-outline " + classes}>
-          {label}{" "}
-          {icon ? (
-            <span className="material-symbols-rounded" aria-hidden="true">
-              {icon}
-            </span>
-          ) : undefined}
-        </a>
-      );
-    } else {
-      return (
-        <button className={"btn btn-outline " + classes}>
-          {label}
-  
-          {icon ? (
-            <span className="material-symbols-rounded" aria-hidden="true">
-              {icon}
-            </span>
-          ) : undefined}
-        </button>
-      );
-    }
-  };
-  
-  ButtonOutline.propTypes = {
-    label: PropTypes.string.isRequired,
-    href: PropTypes.string,
-    target: PropTypes.string,
-    icon: PropTypes.string,
-    classes: PropTypes.string,
-  };
+  if (href) {
+    return (
+      <a href={href} target={target} className={"btn btn-outline " + classes}>
+        {label}{" "}
+        {icon ? (
+          <span className="material-symbols-rounded" aria-hidden="true">
+            {icon}
+          </span>
+        ) : undefined}
+      </a>
+    );
+  } else {
+    return (
+      <button className={"btn btn-outline " + classes}>
+        {label}
 
-  
-export { ButtonPrimary,ButtonOutline };
+        {icon ? (
+          <span className="material-symbols-rounded" aria-hidden="true">
+            {icon}
+          </span>
+        ) : undefined}
+      </button>
+    );
+  }
+};
+
+ButtonOutline.propTypes = {
+  label: PropTypes.string.isRequired,
+  href: PropTypes.string,
+  target: PropTypes.string,
+  icon: PropTypes.string,
+  classes: PropTypes.string,
+};
+
+export { ButtonPrimary, ButtonOutline };
