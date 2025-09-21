45221e89ae4a137ae32e4b5e84c2f836fd4f14b3
siam114 <smsiamislam598@gmail.com>
2025-09-22 00:32:21 +0600

    formating

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,8 +1,6 @@
 import works from "../utils/ProjectsData";
 import ProjectsCard from "./ProjectsCard";
 
-
-
 const Work = () => {
   return (
     <section className="section" id="work">
@@ -10,12 +8,8 @@ const Work = () => {
         <h2 className="headline-2 mb-8 reveal -up">My Portfolio Highlights</h2>
 
         <div className="grid gap-x-4 gap-y-5 grid-cols-[repeat(auto-fill,_minmax(280px,_1fr))]">
-          {works.map(( work, key) => (
-            <ProjectsCard
-              key={key}
-              project={work}
-              classes="reveal -up"
-            />
+          {works.map((work, key) => (
+            <ProjectsCard key={key} project={work} classes="reveal -up" />
           ))}
         </div>
       </div>
