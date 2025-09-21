93f807b2a79370b96afd10f08c516d22ee28d874
siam114 <smsiamislam598@gmail.com>
2025-09-22 00:43:38 +0600

    formatting project data

--- src/utils/ProjectsData.js
+++ src/utils/ProjectsData.js
@@ -1,64 +1,70 @@
 const works = [
-    {
-        id: 1,
-      imgSrc: "/project-0.png",
-      title: "EstateHive Project",
-      tags: ["Express", "MongoDB", "REACT"],
-      projectLink: "https://estatehive-357ff.web.app/",
-      clientLink: "https://github.com/siam114/EstateHive",
-      serverLink: "https://github.com/siam114/EstateHive-server",
-      description: "A modern real estate platform for buying, selling, and managing properties. Users can explore detailed listings with images and descriptions. Features include advanced search, agent profiles, and property management tools."
-    },
-    {
-        id: 2,
-      imgSrc: "/project-1.png",
-      title: "SportAxis Project",
-      tags: ["MongoDB", "Firebase", "Vercel"],
-      projectLink: "https://sportaxis-project.web.app/",
-      clientLink: "https://github.com/siam114/sportsAxios",
-      serverLink: "https://github.com/siam114/SportAxis-server",
-      description: "An interactive sports platform featuring live scores, match schedules, and player statistics. Users can track their favorite teams and get real-time updates. The platform integrates with APIs for live data synchronization."
-    },
-    {
-        id: 3,
-      imgSrc: "/project-2.png",
-      title: "QueryMate Project",
-      tags: ["REACT", "Firebase", "Vercel"],
-      projectLink: "https://querymate-project.web.app/",
-      clientLink: "https://github.com/siam114/QueyMate-",
-      serverLink: "https://github.com/siam114/QueryMate-server",
-      description: "A collaborative Q&A platform designed for developers and tech enthusiasts. Users can ask questions, post answers, and upvote valuable contributions. Firebase authentication ensures secure login and profile management."
-    },
-    {
-        id: 4,
-      imgSrc: "/project-3.png",
-      title: "Job Portal",
-      tags: ["JS", "NodeJS", "Express"],
-      projectLink: "https://job-portal-462b3.web.app/",
-      clientLink: "https://github.com/siam114/job-portal",
-      serverLink: "https://github.com/siam114/job-task-server",
-      description: "A job searching platform connecting job seekers with top companies. Users can upload resumes, apply for jobs, and receive notifications. Employers can post jobs, filter candidates, and manage applications efficiently."
-    },
-    {
-        id: 5,
-      imgSrc: "/project-4.png",
-      title: "CareerGuide Project",
-      tags: ["Tailwind", "DaisyUI", "REACT"],
-      projectLink: "https://careerguide-project.web.app/",
-      clientLink: "https://github.com/siam114/Career-Counseling-project",
-      serverLink: "https://github.com/siam114/Career-Counseling-project",
-      description: "An educational platform offering career advice, mentorship, and skill assessments. Users can explore courses, industry insights, and personalized recommendations. The platform helps individuals plan and advance their careers."
-    },
-    {
-        id: 6,
-      imgSrc: "/project-5.png",
-      title: "Dragon News Category",
-      tags: ["REACT", "NodeJS", "Vercel"],
-      projectLink: "https://dragon-news-7104a.web.app/category/01",
-      clientLink: "https://github.com/siam114/Dragon-news",
-      serverLink: "https://github.com/siam114/Dragon-news",
-      description: "A dynamic news platform delivering category-based news updates. Users can browse trending articles, save favorites, and share content. The system fetches real-time news from various sources for accurate reporting."
-    }
-  ];
+  {
+    id: 1,
+    imgSrc: "/project-0.png",
+    title: "EstateHive Project",
+    tags: ["Express", "MongoDB", "REACT"],
+    projectLink: "https://estatehive-357ff.web.app/",
+    clientLink: "https://github.com/siam114/EstateHive",
+    serverLink: "https://github.com/siam114/EstateHive-server",
+    description:
+      "A modern real estate platform for buying, selling, and managing properties. Users can explore detailed listings with images and descriptions. Features include advanced search, agent profiles, and property management tools.",
+  },
+  {
+    id: 2,
+    imgSrc: "/project-1.png",
+    title: "SportAxis Project",
+    tags: ["MongoDB", "Firebase", "Vercel"],
+    projectLink: "https://sportaxis-project.web.app/",
+    clientLink: "https://github.com/siam114/sportsAxios",
+    serverLink: "https://github.com/siam114/SportAxis-server",
+    description:
+      "An interactive sports platform featuring live scores, match schedules, and player statistics. Users can track their favorite teams and get real-time updates. The platform integrates with APIs for live data synchronization.",
+  },
+  {
+    id: 3,
+    imgSrc: "/project-2.png",
+    title: "QueryMate Project",
+    tags: ["REACT", "Firebase", "Vercel"],
+    projectLink: "https://querymate-project.web.app/",
+    clientLink: "https://github.com/siam114/QueyMate-",
+    serverLink: "https://github.com/siam114/QueryMate-server",
+    description:
+      "A collaborative Q&A platform designed for developers and tech enthusiasts. Users can ask questions, post answers, and upvote valuable contributions. Firebase authentication ensures secure login and profile management.",
+  },
+  {
+    id: 4,
+    imgSrc: "/project-3.png",
+    title: "Job Portal",
+    tags: ["JS", "NodeJS", "Express"],
+    projectLink: "https://job-portal-462b3.web.app/",
+    clientLink: "https://github.com/siam114/job-portal",
+    serverLink: "https://github.com/siam114/job-task-server",
+    description:
+      "A job searching platform connecting job seekers with top companies. Users can upload resumes, apply for jobs, and receive notifications. Employers can post jobs, filter candidates, and manage applications efficiently.",
+  },
+  {
+    id: 5,
+    imgSrc: "/project-4.png",
+    title: "CareerGuide Project",
+    tags: ["Tailwind", "DaisyUI", "REACT"],
+    projectLink: "https://careerguide-project.web.app/",
+    clientLink: "https://github.com/siam114/Career-Counseling-project",
+    serverLink: "https://github.com/siam114/Career-Counseling-project",
+    description:
+      "An educational platform offering career advice, mentorship, and skill assessments. Users can explore courses, industry insights, and personalized recommendations. The platform helps individuals plan and advance their careers.",
+  },
+  {
+    id: 6,
+    imgSrc: "/project-5.png",
+    title: "Dragon News Category",
+    tags: ["REACT", "NodeJS", "Vercel"],
+    projectLink: "https://dragon-news-7104a.web.app/category/01",
+    clientLink: "https://github.com/siam114/Dragon-news",
+    serverLink: "https://github.com/siam114/Dragon-news",
+    description:
+      "A dynamic news platform delivering category-based news updates. Users can browse trending articles, save favorites, and share content. The system fetches real-time news from various sources for accurate reporting.",
+  },
+];
 
-  export default works
+export default works;
